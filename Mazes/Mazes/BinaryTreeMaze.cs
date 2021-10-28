b1a63711c32ce0d8e492cad251de19da5130f4c3
Szymon Murawski <szymon.murawski@wsb.poznan.pl>
2021-10-28 13:47:37 +0200

    feat: Added BinaryTree maze generator

---
+++ Mazes/Mazes/BinaryTreeMaze.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mazes
+{
+    class BinaryTreeMaze
+    {
+        public static Grid Generate(Grid grid)
+        {
+            Random rand = new Random();
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+
+                    List<Cell> neighbours = new List<Cell>();
+                    if (grid.CellsGrid[x, y].North != null)
+                    {
+                        neighbours.Add(grid.CellsGrid[x, y].North);
+                    }
+                    if (grid.CellsGrid[x, y].East != null)
+                    {
+                        neighbours.Add(grid.CellsGrid[x, y].East);
+                    }
+
+                    if(neighbours.Count > 0)
+                    {
+                        Cell neighbour = neighbours[rand.Next(0, neighbours.Count)];
+                        grid.CellsGrid[x, y].LinkCell(neighbour);
+                    }
+                }
+            }
+
+            return grid;
+        }
+    }
+}
