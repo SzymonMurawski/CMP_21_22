2b6e5ba6f102a7527604ca09ad225648d011b64e
Szymon Murawski <szymon.murawski@wsb.poznan.pl>
2021-10-28 12:12:13 +0200

    feat: Cell and Grid classes

---
+++ Mazes/Mazes/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mazes
+{
+    class Cell
+    {
+        public int X;
+        public int Y;
+        public Cell North;
+        public Cell South;
+        public Cell East;
+        public Cell West;
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Mazes/Mazes/Grid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mazes
+{
+    class Grid
+    {
+        public int Width;
+        public int Height;
+        public Cell[,] CellsGrid;
+
+        public Grid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            CellsGrid = new Cell[Width, Height];
+            //TODO initialize every cell in CellsGrid
+        }
+    }
+}
