e309cdbc4053c31c91d32ec6864f3a93334ebe98
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-25 13:40:00 +0100

    feat: Convert input to augmented matrix

--- LinearNumerSystems/LinearNumerSystems/Program.cs
+++ LinearNumerSystems/LinearNumerSystems/Program.cs
@@ -6,10 +6,24 @@ namespace LinearNumerSystems
     {
         static void Main(string[] args)
         {
+            double[,] augmentedMatrix = new double[3,2];
+
             Console.WriteLine("Please type first equation. It should be in the form x1 x2 d.");
             string line1 = Console.ReadLine();
+            // line1 = "121 25 3123";
             Console.WriteLine("Please type second equation. It should be in the form x1 x2 d.");
             string line2 = Console.ReadLine();
+
+            string[] line1Variables = line1.Split(' ');
+            augmentedMatrix[0, 0] = Convert.ToDouble(line1Variables[0]);
+            augmentedMatrix[1, 0] = Convert.ToDouble(line1Variables[1]);
+            augmentedMatrix[2, 0] = Convert.ToDouble(line1Variables[2]);
+
+            string[] line2Variables = line2.Split(' ');
+            augmentedMatrix[0, 1] = Convert.ToDouble(line2Variables[0]);
+            augmentedMatrix[1, 1] = Convert.ToDouble(line2Variables[1]);
+            augmentedMatrix[2, 1] = Convert.ToDouble(line2Variables[2]);
+
             Console.WriteLine("Your equations:");
             Console.WriteLine(line1);
             Console.WriteLine(line2);
