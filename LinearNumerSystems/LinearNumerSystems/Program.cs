6afb4654fa39472425e33fae1ff16952a4c42823
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-25 13:34:24 +0100

    feat: Read input

--- LinearNumerSystems/LinearNumerSystems/Program.cs
+++ LinearNumerSystems/LinearNumerSystems/Program.cs
@@ -6,7 +6,13 @@ namespace LinearNumerSystems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please type first equation. It should be in the form x1 x2 d.");
+            string line1 = Console.ReadLine();
+            Console.WriteLine("Please type second equation. It should be in the form x1 x2 d.");
+            string line2 = Console.ReadLine();
+            Console.WriteLine("Your equations:");
+            Console.WriteLine(line1);
+            Console.WriteLine(line2);
         }
     }
 }
