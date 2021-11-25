48382f091eb1eaa706ecf7401849bce2218785e5
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-25 13:47:40 +0100

    todo: Add todos

--- LinearNumerSystems/LinearNumerSystems/Program.cs
+++ LinearNumerSystems/LinearNumerSystems/Program.cs
@@ -12,6 +12,7 @@ namespace LinearNumerSystems
 
         private static void PrintEquations(double[,] augmentedMatrix)
         {
+            //TODO: Implement better printing algorithm
             Console.WriteLine("Your equations:");
             Console.WriteLine(augmentedMatrix);
         }
@@ -23,8 +24,10 @@ namespace LinearNumerSystems
             for (int eqNo = 0; eqNo < 2; eqNo++)
             {
                 Console.WriteLine($"Please type #{eqNo + 1} equation. It should be in the form x1 x2 d.");
+                //TODO: Change the display, so it is more like in the task
                 string line = Console.ReadLine();
                 string[] lineVariables = line.Split(' ');
+                //TODO: Refactor and change into loop
                 augmentedMatrix[0, eqNo] = Convert.ToDouble(lineVariables[0]);
                 augmentedMatrix[1, eqNo] = Convert.ToDouble(lineVariables[1]);
                 augmentedMatrix[2, eqNo] = Convert.ToDouble(lineVariables[2]);
