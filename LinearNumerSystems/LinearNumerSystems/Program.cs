83206e4153e0874bd8a8e3f587e795bed8523d83
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-25 13:30:44 +0100

    Init

---
+++ LinearNumerSystems/LinearNumerSystems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinearNumerSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
