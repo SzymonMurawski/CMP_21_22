e945cbbd924a3db64b77d53a8281f1b2391acc4e
Szymon Murawski <szymon.murawski@gmail.com>
2021-12-09 11:49:35 +0100

    refactor: Change the order of functions and add some comments

--- LinearNumerSystems/LinearNumerSystems/Program.cs
+++ LinearNumerSystems/LinearNumerSystems/Program.cs
@@ -12,17 +12,30 @@ namespace LinearNumerSystems
             double[] results = BacktrackSubstitution(augmentedMatrix);
             PrintResults(results);
         }
-
-        private static void PrintResults(double[] results)
+        private static double[,] ReadEquations()
         {
-            throw new NotImplementedException();
-        }
+            double[,] augmentedMatrix = new double[3, 2];
 
-        private static double[] BacktrackSubstitution(double[,] augmentedMatrix)
+            for (int eqNo = 0; eqNo < 2; eqNo++)
+            {
+                Console.WriteLine($"Please type #{eqNo + 1} equation. It should be in the form x1 x2 y.");
+                //TODO: Change the display, so it is more like in the task
+                string line = Console.ReadLine();
+                string[] lineVariables = line.Split(' ');
+                //TODO: Refactor and change into loop
+                augmentedMatrix[0, eqNo] = Convert.ToDouble(lineVariables[0]);
+                augmentedMatrix[1, eqNo] = Convert.ToDouble(lineVariables[1]);
+                augmentedMatrix[2, eqNo] = Convert.ToDouble(lineVariables[2]);
+            }
+            return augmentedMatrix;
+        }
+        private static void PrintEquations(double[,] augmentedMatrix)
         {
-            throw new NotImplementedException();
+            //TODO: Implement better printing algorithm
+            Console.WriteLine("Your equations:");
+            Console.WriteLine($"Eq #1: {augmentedMatrix[0, 0]}*x1 + {augmentedMatrix[1, 0]}*x2 = {augmentedMatrix[2, 0]}");
+            Console.WriteLine($"Eq #2: {augmentedMatrix[0, 1]}*x1 + {augmentedMatrix[1, 1]}*x2 = {augmentedMatrix[2, 1]}");
         }
-
         private static void ForwardElimination(double[,] augmentedMatrix)
         {
             //TODO: extract into method
@@ -40,32 +53,18 @@ namespace LinearNumerSystems
                 }
             }
         }
-
-        private static void PrintEquations(double[,] augmentedMatrix)
+        private static double[] BacktrackSubstitution(double[,] augmentedMatrix)
         {
-            //TODO: Implement better printing algorithm
-            Console.WriteLine("Your equations:");
-            Console.WriteLine($"Eq #1: {augmentedMatrix[0,0]}*x1 + {augmentedMatrix[1, 0]}*x2 = {augmentedMatrix[2, 0]}");
-            Console.WriteLine($"Eq #2: {augmentedMatrix[0, 1]}*x1 + {augmentedMatrix[1, 1]}*x2 = {augmentedMatrix[2, 1]}");
+            //TODO: Implement this function. It should perform the backtrack substitution on the augmented matrix and return the total result - variables and their values
+            throw new NotImplementedException();
         }
 
-        private static double[,] ReadEquations()
+        private static void PrintResults(double[] results)
         {
-            double[,] augmentedMatrix = new double[3, 2];
-
-            for (int eqNo = 0; eqNo < 2; eqNo++)
-            {
-                Console.WriteLine($"Please type #{eqNo + 1} equation. It should be in the form x1 x2 d.");
-                //TODO: Change the display, so it is more like in the task
-                string line = Console.ReadLine();
-                string[] lineVariables = line.Split(' ');
-                //TODO: Refactor and change into loop
-                augmentedMatrix[0, eqNo] = Convert.ToDouble(lineVariables[0]);
-                augmentedMatrix[1, eqNo] = Convert.ToDouble(lineVariables[1]);
-                augmentedMatrix[2, eqNo] = Convert.ToDouble(lineVariables[2]);
-            }
-
-            return augmentedMatrix;
+            // TODO implement this function. It should print to the user the results in a nice looking way, for example something like:
+            // x1 = 123
+            // x2 = 456
+            throw new NotImplementedException();
         }
     }
 }
